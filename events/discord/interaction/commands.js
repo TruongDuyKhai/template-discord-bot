820e5c8b79d4d20a121de6d3466cd76db7461ff5
TruongDuyKhai <truongduykhai1012@gmail.com>
2025-03-11 19:17:45 +0700

    .

--- events/discord/interaction/commands.js
+++ events/discord/interaction/commands.js
@@ -1,16 +1,27 @@
 module.exports = {
-    name: 'interactionCreate',
+    name: "interactionCreate",
     async execute(client, interaction) {
-        if (!interaction.isCommand()) return;
-        if(!interaction.guild) return;
-        const command = client.slashCommands.find(e => e.data.name === interaction.commandName);
-        if (!command) {
-            return interaction.reply({
-                ephemeral: true,
-                content: 'Không tìm thấy lệnh, vui lòng thử lại sau.'
-            })
+        if (!interaction.guild) return;
+        if (interaction.isChatInputCommand()) {
+            const command = client.slashCommands.find(
+                (e) => e.data.name === interaction.commandName
+            );
+            if (!command) {
+                return interaction.reply({
+                    ephemeral: true,
+                    content: "Không tìm thấy lệnh, vui lòng thử lại sau.",
+                });
+            }
+            if (command.deferReply)
+                await interaction.deferReply(command.deferReply);
+            await command.execute(client, interaction);
         }
-        if (command.deferReply) await interaction.deferReply(command.deferReply);
-        await command.execute(client, interaction);
-    }
-}
+        if (interaction.isAutocomplete()) {
+            const command = client.slashCommands.find(
+                (e) => e.data.name === interaction.commandName
+            );
+            if (!command) return;
+            await command.autoComplete(client, interaction);
+        }
+    },
+};
