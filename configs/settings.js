457421a94b56c33d44046d7de7eecfb26c25ac60
TruongDuyKhai <truongduykhai1012@gmail.com>
2025-04-01 12:48:07 +0700

    .

--- configs/settings.js
+++ configs/settings.js
@@ -2,5 +2,5 @@ module.exports = {
     token: process.env.TOKEN || "",
     devUsers: ["871329074046435338", "1133037157527859230"],
     port: process.env.PORT || 3000,
-    guildId: "1354085850538967152",
+    guildId: "",
 };
