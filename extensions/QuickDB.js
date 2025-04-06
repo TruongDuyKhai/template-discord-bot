dd6bd7275e26f5c3e57ece9b6851373cc21c35e3
TruongDuyKhai <truongduykhai1012@gmail.com>
2025-04-06 15:23:47 +0700

    .

--- extensions/QuickDB.js
+++ extensions/QuickDB.js
@@ -1,5 +1,5 @@
 const { QuickDB } = require("quick.db");
-const nanoid = require("nanoid");
+const {nanoid} = require("nanoid");
 
 class QuickDBExtension extends QuickDB {
     constructor(options) {
