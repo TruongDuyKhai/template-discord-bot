ddb6d05cf601c1ec5cf82534028015295a0c5f4b
TruongDuyKhai <truongduykhai1012@gmail.com>
2025-09-21 00:36:20 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const discord = require("discord.js");
-const { Partials } = discord;
+const { Partials, Options } = discord;
 const Client = require("./extensions/discordClient");
 
 const client = new Client({
@@ -10,6 +10,41 @@ const client = new Client({
     allowedMentions: {
         repliedUser: false,
         parse: ["users"],
+    },
+    sweepers: {
+        ...Options.DefaultSweeperSettings,
+        messages: {
+            interval: 3600,
+            filter: () => (message) => true,
+        },
+        users: {
+            interval: 3600,
+            filter: () => (user) => user.bot && user.id !== user.client.user.id,
+        },
+        invites: {
+            interval: 3600,
+            filter: () => (invite) => true,
+        },
+        users: {
+            interval: 3600,
+            filter: () => (user) => true,
+        },
+        presences: {
+            interval: 3600,
+            filter: () => (presence) => true,
+        },
+        reactions: {
+            interval: 3600,
+            filter: () => (reaction) => true,
+        },
+        emojis: {
+            interval: 3600,
+            filter: () => (emoji) => true,
+        },
+        bans: {
+            interval: 3600,
+            filter: () => (ban) => true,
+        },
     },
     token: require("./configs/settings").token,
 });
