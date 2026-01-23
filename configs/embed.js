ad31fe58dae6121507e40128d09c810d9de5a7ec
TruongDuyKhai <truongduykhai1012@gmail.com>
2026-01-23 21:47:34 +0700

    .

--- configs/embed.js
+++ configs/embed.js
@@ -1,3 +1,3 @@
 module.exports = {
-    color: "#ff00aa",
+    color: "#ffffff",
 };
--- configs/settings.js
+++ configs/settings.js
@@ -1,12 +1,10 @@
 module.exports = {
     token: process.env.TOKEN || "",
     devUserIds: ["871329074046435338", "1133037157527859230"],
-    ownerUserIds: ["1320262466407501846"],
+    ownerUserIds: [],
     port: process.env.PORT || 3000,
-    guildId: "1461373514333290787",
-    inviteCode: "E9jAS79m9G",
+    guildId: "",
+    inviteCode: "",
     textCommands: false,
     prefix: "",
-
-    welcomeChannelId: "1461582748966719653",
 };
